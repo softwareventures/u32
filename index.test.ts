7aa38bf6986938297ed37e7d7a4f96fb2770f030
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-09-24 22:29:53 +0100

    feat(ucmp): add ucmp function

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import {unot} from "./index";
+import {ucmp, unot} from "./index";
 
 test("unot", t => {
     t.is(unot(0), 1);
@@ -7,4 +7,15 @@ test("unot", t => {
     t.is(unot(-1), 0);
     t.is(unot(342), 0);
     t.is(unot(-342), 0);
+});
+
+test("ucmp", t => {
+    t.is(ucmp(0), 0xffffffff);
+    t.is(ucmp(1), 0xfffffffe);
+    t.is(ucmp(-1), 0);
+    t.is(ucmp(0xffffffff), 0);
+    t.is(ucmp(0xfffffffe), 1);
+    t.is(ucmp(-0x3abc5126), 0x3abc5125);
+    t.is(ucmp(0xc543aeda), 0x3abc5125);
+    t.is(ucmp(0x3abc5125), 0xc543aeda);
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import {inot as unot} from "i32";
 
 export {unot};
+
+export function ucmp(value: number): number {
+    return (~value) >>> 0;
+}
